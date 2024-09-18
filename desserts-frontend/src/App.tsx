2cdd5aa23f2f7e3592c2db7071bcc04751b82208
Kissdani05 <kissdani05@gmail.com>
2024-09-18 14:28:21 +0200

    kész

---
+++ desserts-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import DessertCard from "./components/DessertCard"
+
+const App = () => {
+  return (
+    <div>
+      <DessertCard name={"Waffle with Berries"}/>
+      <DessertCard name={"Macaron Mix of Five"}/>
+      <DessertCard name={"Classic Tiramisu"}/>
+    </div>
+  )
+}
+
+export default App
